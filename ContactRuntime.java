9b10ee0caaab2f3bb7f1040072df99136cb1456a
BenTibnam <BenTibnam@users.noreply.github.com>
2018-07-19 23:01:49 -0400

    Delete ContactRuntime.java

--- ContactRuntime.java
+++
@@ -1,7 +0,0 @@
-public class ContactRuntime{
-    public static ContactManager cm = new ContactManager();
-
-    public static void main(String args[]){
-        WindowManager.entryWindow();
-    }
-}
